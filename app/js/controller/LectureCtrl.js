dd60f21c03806f762de61b9523f982bbf450f085
Mike Kocijevsky <m.kocijevsky@gmail.com>
2013-10-13 21:26:07 +0300

    Show Comments per lesson

--- app/js/controller/LectureCtrl.js
+++ app/js/controller/LectureCtrl.js
@@ -6,4 +6,5 @@ APP.Ctrls.LectureCtrl = function ($scope, $routeParams) {
     $scope.id = parseInt($routeParams.id);
     $scope.lecture = APP.Data.getLecture($scope.id);
     $scope.teacher = APP.Data.getHuman($scope.lecture.teacher_id);
+    $scope.comments = APP.Data.Tables.comments;
 };
