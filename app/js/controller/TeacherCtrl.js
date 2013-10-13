221d4a5ce599fcaf04915b292165a37cefc6b62d
Mike Kocijevsky <m.kocijevsky@gmail.com>
2013-10-13 12:34:23 +0300

    Redesigned teachers page - quotes block remade - added delete/edit functionality from single teacher view

--- app/js/controller/TeacherCtrl.js
+++ app/js/controller/TeacherCtrl.js
@@ -2,7 +2,19 @@
  * This is controller of the single Teacher page
  * @name TeacherCtrl
  */
-APP.Ctrls.TeacherCtrl = function ($scope, $routeParams) {
+APP.Ctrls.TeacherCtrl = function ($scope, $routeParams, $location) {//TODO:conformation on delete
     $scope.id = parseInt($routeParams.id);
     $scope.human = APP.Data.getHuman($scope.id);
+
+    /**
+     * Removes teacher from APP.Data.Tables.people
+     * @name remove
+     * @param {string} id
+     * redirection after remove
+     */
+    $scope.remove = function (id) {
+        APP.Data.removeHuman(id);
+        $scope.newURI = "/teachers/";
+        $location.path($scope.newURI);
+    };
 };
