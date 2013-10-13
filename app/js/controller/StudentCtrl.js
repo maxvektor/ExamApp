692eb822d1baf0dfe74571019331c8c89de4f724
Mike Kocijevsky <m.kocijevsky@gmail.com>
2013-10-13 11:36:48 +0300

    Students page redesigned - ng-hide options added - map with photo added(using Yandex map)

--- app/js/controller/StudentCtrl.js
+++ app/js/controller/StudentCtrl.js
@@ -5,9 +5,46 @@
 APP.Ctrls.StudentCtrl = function ($scope, $routeParams) {
     $scope.id = parseInt($routeParams.id);
     $scope.human = APP.Data.getHuman($scope.id);//TODO: check for presents of the object fields
-    $scope.city = $scope.human.city;
-    $scope.fb = $scope.human.social.fb;
-    $scope.vk = $scope.human.social.vk;
-    $scope.git = $scope.human.social.git;
-    $scope.ya = $scope.human.social.ya;
+    if (APP.Data.getCity($scope.human.city)) {
+        $scope.city = APP.Data.getCity($scope.human.city).name;
+    } else {
+        $scope.city = null;
+    }
+    if ($scope.human && $scope.human.social) {
+        $scope.fb = $scope.human.social.fb;
+        $scope.vk = $scope.human.social.vk;
+        $scope.git = $scope.human.social.git;
+        $scope.ya = $scope.human.social.ya;
+    }
+
+    /**
+     * This function initilizing map on the page if ymaps is redy
+     * @name initializeMap
+     * calls on ng-init
+     */
+    $scope.initializeMap = function () {
+        var mapOptions = {
+                center: [50.5, 30.5],
+                zoom: 11
+            },
+            mapContainer = "map";
+
+        ymaps.ready(function () {
+            ymaps.geocode($scope.city, { results: 1 }).then(
+                function (res) {
+                    var coords = res.geoObjects.get(0).geometry.getCoordinates(),
+                        map;
+                    mapOptions.center = coords;
+                    $scope.map = new ymaps.Map(mapContainer, mapOptions);
+                    myPlacemark1 = new ymaps.Placemark(coords,
+                        {
+                            iconContent: "<img src='" + $scope.human.photos.m + "'>"
+                        },
+                        {
+                            preset: 'twirl#redStretchyIcon'
+                        });
+                    $scope.map.geoObjects.add(myPlacemark1);
+                });
+        });
+    };
 };
